03d10754c8b3deaecaf29f1b87cc8d45335628e1
Jorge Villalobos <jorgemvc@gmail.com>
2018-03-25 10:22:33 -0600

    Create SimpleTest.ino

---
+++ examples/SimpleTest/SimpleTest.ino
@@ -0,0 +1,37 @@
+/** @file    SimpleTest.h
+ *  @author  Jorge Villalobos Carvajal (jorgemvc@gmail.com)
+ *  @date    23.Mar.2018  
+ *  @version 1.0 
+ *  
+ *  @brief   Simple example for S4ALib usage.
+ *
+ *  @licence  MIT License
+ */
+#include <S4ALib.h>
+
+S4ALib Robot;
+
+void setup() {
+  Robot.begin();
+}  // setup
+
+void loop() {
+  Robot.ledOn();
+
+  Robot.setSpeed(150, 150);
+  Robot.forward(500);
+
+  Robot.setSpeed(150, 150);
+  Robot.backward(00);
+  
+  Robot.setSpeed(150, 150);
+  Robot.left(500);
+
+  Robot.setSpeed(150, 150);
+  Robot.right(500);
+
+  Robot.ledOff();
+
+  for (int i = 0 ; i < 5 ; i++)
+    Robot.blink(200);
+}  // loop
